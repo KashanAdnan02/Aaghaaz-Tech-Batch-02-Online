b503943ad94040704f79e48d79d0728d53356abf
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-01 07:49:14 -0700

    javscript 3rd class code

---
+++ Module 2/class 22/script.js
@@ -0,0 +1,23 @@
+// single line comment
+
+// alert("Hello I am Kashan Adnan from Aaghaaz Tech")
+// window.alert("Hello")
+
+// hello
+
+// multiline comments
+/*
+hello
+and 
+aasdk
+asdflkasdf
+asdlfkasd
+asdlfasd
+asdfkasd
+asdflkasdf
+asdfkasdfk
+*/
+
+var kas_han = 9
+
+
