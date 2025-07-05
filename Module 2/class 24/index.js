c1b985f70058f199707ee9214d8d1eb7c22bc6b9
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-05 07:29:44 -0700

    if statement class 7/5/2025

---
+++ Module 2/class 24/index.js
@@ -0,0 +1,32 @@
+
+// condition // true
+
+// false 7 == 7
+// if(7 == 7){
+// code execute
+//     alert("I am Right")
+// }
+
+
+// alert("Heloo I am kashan adnan")
+// code run 
+
+// 7 == 7 // true
+// kashan === kashan 
+// "7" == 7
+// if ("7" === 7) {
+//     alert("Hi, i am haris") // 
+// }
+// == 
+// === // value, data_type
+
+// shoaib / false
+// arisha / false
+// kamran / false
+
+
+alert(7 + 7) // 14
+alert(7 * 7) // 49
+alert(7 / 7) // 1
+alert(7 % 13) // 6
+alert(2 ** 4) // 2 * 2 * 2 * 2 // 16
