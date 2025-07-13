a3149246ae9610f87ae4831eda43847bcdca7e23
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-13 08:10:12 -0700

    if statement and array

---
+++ Module 2/class 25/script.js
@@ -0,0 +1,34 @@
+// var name = "Kashan"
+
+// // wrong ya hello
+// if (name === "Kashan") {
+//     alert("Hello")
+// } else if (name === "Shoaib") {
+//     alert("It works")
+// } else if (name === "Ariz") {
+//     alert("I am working")
+// } else if (name === "Kamran") {
+//     alert("I am also working")
+// } else {
+//     alert("Wrong")
+// }
+
+
+var name = "Kashan"
+var age = 29
+//                  0               1       2
+var courses = ["Web Development", "Ai", "Graphic"]
+//                0        1          2           3  
+var students = ["Ariz", "Kamran", "Shoaib", "Arisha Fatima"]
+//                0         1         2
+var classes = ["2 to 5", "5 to 6", "7 to 8"]
+// completed
+// review
+// in-progress
+
+prompt("Enter your name")
+prompt("Enter your progress")
+alert(students[2])
+
+alert(courses)
+courses[1] = "Ai & Data Science"
