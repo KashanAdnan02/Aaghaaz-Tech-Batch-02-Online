1d11eff034fb9c5da33d86750a48cd4e5fec1d32
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-08-10 19:37:42 +0500

    done

---
+++ Module 2/class 27/script.js
@@ -0,0 +1,43 @@
+// const students = ["Yahya", "Shoaib", "Kamran","Ariz"]
+
+// var userinp = prompt("Enter any letter to find a name in the student list?")
+// console.log(userinp);
+
+// // declaration condition icrement
+// for(var i = 0; i < students.length; i += 1){
+//     if (students[i].indexOf(userinp) === -1) {
+//         alert("The letter does not exist in this words")
+//     }else{
+//         console.log(students[i])
+//     }
+// }
+
+
+
+// function printWordFromLetters
+
+
+
+// // what are functions?
+//     //                           lenght = 4
+//     //              0         1         2       3
+// const teachers = ["Kashan", "Sir"]
+// // for loop
+
+// var userinp2 = prompt("Enter any letter to find a name in the student list?")
+// console.log(userinp2);
+
+// // declaration condition icrement
+// for(var i = 0; i < teachers.length; i += 1){
+//     if (teachers[i].indexOf(userinp2) === -1) {
+//         alert("The letter does not exist in this words")
+//     }else{
+//         console.log(teachers[i])
+//     }
+// }
+
+
+// "Yahya".indexOf("k") // -1/ 
+
+
+
