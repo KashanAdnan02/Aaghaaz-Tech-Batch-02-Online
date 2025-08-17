441055bc599fc4761d5f5f0796b5a9cb11278e47
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-08-17 19:55:19 +0500

    done

---
+++ Module 2/class 28/script.js
@@ -0,0 +1,17 @@
+// var num1 = prompt("enter your num1: ")
+// var num2 = prompt("enter your num2: ")
+
+// var sum = num1 - num2
+
+// alert(sum)
+
+function subtract() {
+    var num1 = prompt("enter your num1: ")
+    var num2 = prompt("enter your num2: ")
+
+    var sum = num1 - num2
+
+    alert(sum)
+}
+
+subtract();
