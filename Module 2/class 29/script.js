6c8618642967d6c429e7becd0865eeea28ef4334
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-09-13 07:50:09 -0700

    done

---
+++ Module 2/class 29/script.js
@@ -0,0 +1,53 @@
+// Today our topic is switch statement
+
+
+// if statement
+// name == "Kashan" // if statment
+// if(condition){
+
+// }
+
+// let dayOfWeek = "Tue" // prompt "Fri" , "Sat", "Sun", "Mon"
+// kuch bhi nahi
+// error
+
+// km = error
+// sh = kuch nhi // right
+// ar = error
+// switch(dayOfWeek){
+//     case "Sat":
+//         console.log("Hello")
+//         break;
+//     case "Mon":
+//         console.log("There is alot of work")
+//         break;
+//     case "Fri":
+//         console.log("Fun Day!")
+//         break
+//     default:
+//         console.log("It is not provided in this switch case statment")
+// }
+
+// Consoled // Fun Day
+
+// for loop 
+
+// while loop
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// var i = 10
+// do{
+//     console.log(i) // true
+//     i++
+// }while(i < 10) // false
+
+// 1 9
+// 0 10
+// 1 10
+// 0 9
+
+// 1
+// 2
